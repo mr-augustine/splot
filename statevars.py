2f0be19dcec6d22580a8209de1a6233164d11255
Augustine <pushbuttonrobotics@gmail.com>
2016-08-07 15:57:51 -0500

    Cleanup statevars.py
    I added comments to all methods; the methods were rearranged; minor
    variable renaming

--- statevars.py
+++ statevars.py
@@ -1,7 +1,7 @@
 class Statevars:
-    'Class responsible for interpreting the statevars definition file'
+    """ Class responsible for parsing the statevars definition file """
 
-    DELIMITER = ','
+    FIELD_DELIMETER = ','
     FIELD_DATATYPE_INDEX = 0
     FIELD_COUNT_INDEX = 1
     FIELD_NAME_INDEX = 2
@@ -22,11 +22,115 @@ class Statevars:
         #Open the file
         #Parse the file
         #Populate the list of statevars (type, size, name)
-        if not self.__parse_statevars_file(filename):
+        if not self._parse_statevars_file(filename):
             self._varlist = None
 
-    # You give it a filename and it parses its contents
-    def __parse_statevars_file(self, filename):
+    @staticmethod
+    def get_datatype_size(datatype):
+        """ Returns the size of the specified datatype """
+
+        if datatype in Statevars.VAR_TYPE_SIZE_MAP.keys():
+            return Statevars.VAR_TYPE_SIZE_MAP[datatype]
+
+        return 0
+
+    @staticmethod
+    def get_field_count(field_def):
+        """ Returns the number of contiguous occurences for the
+        specified field
+        """
+
+        return field_def[Statevars.FIELD_COUNT_INDEX]
+
+    @staticmethod
+    def get_field_datatype(field_def):
+        """ Returns the datatype of the specified field """
+
+        return field_def[Statevars.FIELD_DATATYPE_INDEX]
+
+    @staticmethod
+    def get_field_exp_val(field_def):
+        """ Returns the expected value of the specified field """
+
+        return field_def[Statevars.FIELD_EXPECTED_VAL_INDEX]
+
+    @staticmethod
+    def get_field_name(field_def):
+        """ Returns the field name """
+
+        return field_def[Statevars.FIELD_NAME_INDEX]
+
+    @staticmethod
+    def has_exp_val(field_def):
+        """ Returns True if the specified field has an associated
+        expected value
+        """
+
+        return Statevars.get_field_exp_val(field_def) != ''
+
+    def _is_array(self, array_name):
+        """ Returns 1 if the statevar field name is an array of the format:
+        array_name[<array_size>]; Returns 0 otherwise
+        """
+
+        if (array_name.count('[') == 0) or (array_name.count(']') == 0):
+            return 0
+
+        # Verify that the array length is at least one digit wide
+        left_bracket_pos = array_name.find('[')
+        if left_bracket_pos < len(array_name) - 2 and  array_name.endswith(']'):
+            array_element_count = array_name[left_bracket_pos + 1:len(array_name) - 1]
+
+            if array_element_count.isdigit() and int(array_element_count > 0):
+                return 1
+
+        return 0
+
+    def _parse_array(self, array_name):
+        """ Returns a tuple that contains the name and size of the specified
+        array
+        """
+
+        left_bracket_pos = array_name.find('[')
+        array_element_count = array_name[left_bracket_pos + 1 : len(array_name) - 1]
+
+        return (array_name[:left_bracket_pos], int(array_element_count))
+
+    def _parse_datatype(self, datatype):
+        """ Parses the datatype; Returns a tuple containing the datatype name
+        and the count (number of contiguous values of that type)
+        """
+
+        if self._is_array(datatype):
+            (datatype, count) = self._parse_array(datatype)
+        else:
+            count = 1
+
+        if datatype not in self.VAR_TYPE_SIZE_MAP.keys():
+            print 'Unrecognized data type: ' + datatype
+            return ('', '')
+        else:
+            return (datatype, count)
+
+    def _parse_line(self, line):
+        """ Parses the specified line for statevar definition fields;
+        Returns a tuple that contains the datatype, name and expected value
+        """
+
+        if line.count(self.FIELD_DELIMETER) == 1:
+            (datatype, name) = line.split(self.FIELD_DELIMETER, 2)
+            exp_val = ''
+        elif line.count(self.FIELD_DELIMETER) == 2:
+            (datatype, name, exp_val) = line.split(self.FIELD_DELIMETER, 3)
+        else:
+            (datatype, name, exp_val) = ('', '', '')
+            print 'Malformed statevars definition: ' + line
+
+        return (str.strip(datatype), str.strip(name), str.strip(exp_val))
+
+    def _parse_statevars_file(self, filename):
+        """ Parses the specified statevars file """
+
         with open(filename) as f:
             lines = f.readlines()
 
@@ -41,14 +145,14 @@ class Statevars:
                 continue
 
             # datatype and name are mandatory for all lines
-            (datatype, name, exp_val) = self.__parse_line(line)
+            (datatype, name, exp_val) = self._parse_line(line)
 
             if '' in (datatype, name):
                 # There was an error
                 print 'line number: ' + str(line_number)
                 return 0
 
-            (datatype, count) = self.__parse_datatype(datatype)
+            (datatype, count) = self._parse_datatype(datatype)
 
             if '' in (datatype, count):
                 return 0
@@ -57,75 +161,7 @@ class Statevars:
 
         return 1
 
-    def __parse_line(self, line):
-        if line.count(self.DELIMITER) == 1:
-            (datatype, name) = line.split(self.DELIMITER, 2)
-            exp_val = ''
-        elif line.count(self.DELIMITER) == 2:
-            (datatype, name, exp_val) = line.split(self.DELIMITER, 3)
-        else:
-            (datatype, name, exp_val) = ('', '', '')
-            print 'Malformed statevars definition: ' + line
-
-        return (str.strip(datatype), str.strip(name), str.strip(exp_val))
-
-    def __parse_datatype(self, datatype):
-        if self.__is_array(datatype):
-            (datatype, count) = self.__parse_array(datatype)
-        else:
-            count = 1
-
-        if datatype not in self.VAR_TYPE_SIZE_MAP.keys():
-            print 'Unrecognized data type: ' + datatype
-            return ('', '')
-        else:
-            return (datatype, count)
-
-    def __is_array(self, array_name):
-        if (array_name.count('[') == 0) or (array_name.count(']') == 0):
-            return 0
-
-        left_bracket_pos = array_name.find('[')
-        if left_bracket_pos < len(array_name) - 2 and  array_name.endswith(']'):
-            array_element_count = array_name[left_bracket_pos + 1:len(array_name) - 1]
-
-            if array_element_count.isdigit() and int(array_element_count > 0):
-                return 1
-
-        return 0
-
-    def __parse_array(self, array_name):
-        left_bracket_pos = array_name.find('[')
-        array_element_count = array_name[left_bracket_pos + 1 : len(array_name) - 1]
-
-        return (array_name[:left_bracket_pos], int(array_element_count))
-
     def get_varlist(self):
+        """ Returns the list of statevars variables """
+
         return self._varlist
-
-    @staticmethod
-    def get_field_datatype(field_def):
-        return field_def[Statevars.FIELD_DATATYPE_INDEX]
-
-    @staticmethod
-    def get_field_count(field_def):
-        return field_def[Statevars.FIELD_COUNT_INDEX]
-
-    @staticmethod
-    def get_field_name(field_def):
-        return field_def[Statevars.FIELD_NAME_INDEX]
-
-    @staticmethod
-    def get_field_exp_val(field_def):
-        return field_def[Statevars.FIELD_EXPECTED_VAL_INDEX]
-
-    @staticmethod
-    def get_datatype_size(datatype):
-        if datatype in Statevars.VAR_TYPE_SIZE_MAP.keys():
-            return Statevars.VAR_TYPE_SIZE_MAP[datatype]
-
-        return 0
-
-    @staticmethod
-    def has_exp_val(field_def):
-        return Statevars.get_field_exp_val(field_def) != ''
