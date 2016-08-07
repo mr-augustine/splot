29894a919e0b5dc7c97760a34b124326c49b86f5
Augustine <pushbuttonrobotics@gmail.com>
2016-08-07 15:31:45 -0500

    Cleanup data.py
    I added comments to each method; rearranged some methods; and renamed a
    variable

--- data.py
+++ data.py
@@ -3,7 +3,9 @@ import struct
 from statevars import Statevars
 
 class Data:
-    'Stores the data read from the datafile'
+    """ Class responsible for reading and storing state variable data from the
+    specified data file according to the specified state variable definition
+    """
 
     _data_list = []
 
@@ -14,7 +16,7 @@ class Data:
         num_fields = len(statevars.get_varlist())
 
         while True:
-            next_dataframe = self.__read_next_dataframe(datafile, statevars.get_varlist())
+            next_dataframe = self._read_next_dataframe(datafile, statevars.get_varlist())
             if len(next_dataframe) == num_fields:
                 self._data_list.append(next_dataframe)
             elif len(next_dataframe) == 0:
@@ -30,46 +32,11 @@ class Data:
 
         datafile.close()
 
+    def _convert_bytes(self, bytes_to_convert, field_def):
+        """ Returns the specified bytes as a value interpreted in accordance
+        with the field definition
+        """
 
-    def __read_next_dataframe(self, datafile, statevars_def):
-        statevars_index = 0
-        out_hash = {}
-
-        for field in statevars_def:
-            field_size = Statevars.get_datatype_size(Statevars.get_field_datatype(field))
-            field_count = Statevars.get_field_count(field)
-            field_name = Statevars.get_field_name(field)
-
-            buff = datafile.read(field_size * field_count)
-
-            if not buff:
-                out_hash.clear()
-                return out_hash
-
-            # Little Endian conversion is handled in the convert_bytes() function
-            if field_count == 1:
-                converted_buff = self.__convert_bytes(buff, field)
-            # TODO test this code section with multiple, multi-byte values
-            elif field_count > 1:
-                if field_size > 1:
-                    new_buff = ()
-                    for index in range(0, field_count):
-                        new_buff.append(self.__convert_bytes(buff[index], field))
-                    converted_buff = new_buff
-                elif field_size == 1:
-                    converted_buff = str(buff)
-
-            out_hash[field_name] = converted_buff
-
-            # print 'field = ' + str(field)
-            # print 'field size = ' + str(field_size)
-            # print 'field count = ' + str(field_count)
-            # print 'field value = ' + str(out_hash[field_name]) + '\n'
-
-        return out_hash
-            #print Statevars.get_field_name(field) + ": " + str(struct.unpack_from("<I", buff)[0])
-
-    def __convert_bytes(self, bytes_to_convert, field_def):
         datatype = Statevars.get_field_datatype(field_def)
         format_string = 'c'
 
@@ -90,10 +57,9 @@ class Data:
 
         return struct.unpack_from(format_string, bytes_to_convert)[0]
 
-    def get_data(self):
-        return self._data_list
-
     def get_all(self, field_name):
+        """ Returns a list of all recorded values for the specified field """
+
         data = []
 
         for frame in self._data_list:
@@ -101,3 +67,46 @@ class Data:
 
         return data
 
+    def get_data(self):
+        """ Returns the data list of data frames """
+        return self._data_list
+    
+    def _read_next_dataframe(self, datafile, statevars_def):
+        """ Returns a dictionary that contains the field names and associated
+        values for the next data frame in the data file
+        """
+
+        statevars_index = 0
+        data_frame = {}
+
+        for field in statevars_def:
+            field_size = Statevars.get_datatype_size(Statevars.get_field_datatype(field))
+            field_count = Statevars.get_field_count(field)
+            field_name = Statevars.get_field_name(field)
+
+            buff = datafile.read(field_size * field_count)
+
+            if not buff:
+                data_frame.clear()
+                return data_frame
+
+            # Little Endian conversion is handled in the convert_bytes() function
+            if field_count == 1:
+                converted_buff = self._convert_bytes(buff, field)
+            # TODO test this code section with multiple, multi-byte values (e.g., int32[3])
+            elif field_count > 1:
+                # Handle arrays of multi-byte elements
+                if field_size > 1:
+                    new_buff = ()
+                    for index in range(0, field_count):
+                        new_buff.append(self._convert_bytes(buff[index], field))
+                    converted_buff = new_buff
+                # Handle arrays of single-byte elements (e.g., char[16])
+                # TODO update this code to handle multiple, single-byte values
+                # other than char (e.g., int8_t and uint8_t)
+                elif field_size == 1:
+                    converted_buff = str(buff)
+
+            data_frame[field_name] = converted_buff
+
+        return data_frame
